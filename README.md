943266ec32dee069e7defee500257ebbfdf16951
Armando Locay <alocay@gmail.com>
2018-05-19 10:17:41 -0400

    updating steamauth settings and adding readme

---
+++ README.md
@@ -0,0 +1,13 @@
+What Could We Play? (WCWP)
+
+A very basic project (mainly as a personal test bed/learning tool for PHP). The main idea was to use the Steam APIs to gather information about 
+the games that you and another friend(s) own to determine what the two of you could play together.
+
+To run, download XAMPP, place the project contents in its own directory under [XAMPP directory]/htdocs, and start the Apache server. Navigating to `localhost/[directory]` should do the trick.  
+**Note:** The domain name needs to be changed in the `settings.php` under the `steamauth` directory.
+
+Demo coming eventually.
+
+## License
+Copyright (c) 2015 Armando Locay
+Licensed under the MIT license.
--- steamauth/settings.php
+++ steamauth/settings.php
@@ -1,6 +1,6 @@
 <?php
 $steamauth['apikey'] = "8B8C86D67AB89F6F4F7CD347003F7CF5";
-$steamauth['domainname'] = "armandolocay.com"; // The main URL of your website displayed in the login page
+$steamauth['domainname'] = "[ENTER_DOMAIN_NAME]"; // The main URL of your website displayed in the login page
 $steamauth['buttonstyle'] = "large"; // Style of the login button [small|large_no|large]
 $steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = "/wcwp"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
